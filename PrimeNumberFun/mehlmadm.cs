485b7a8f02608bd7e5787632940c81882bc63468
Andrew Mehlman <mehlmadm@mail.uc.edu>
2024-04-02 11:26:35 -0400

    Add a constructor to the new class name after my 6+2

---
+++ PrimeNumberFun/mehlmadm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    internal class mehlmadm
+    {
+        // Add a constructor that prints "Hello"
+        public mehlmadm() {
+            Console.WriteLine("Hello "); 
+                }
+    }
+}
