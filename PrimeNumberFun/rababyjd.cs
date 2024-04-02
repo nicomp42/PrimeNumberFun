7ab38a16f81c3166941c596b73519989d62a84de
Jared Rababy <jaredrababy@gmail.com>
2024-04-02 11:26:18 -0400

    Add a constructor to the new class named after my 6+2

---
+++ PrimeNumberFun/rababyjd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    internal class rababyjd
+    {
+        // Add a constructior that prints "Hello"
+        public rababyjd() {
+            Console.WriteLine("Hello");
+        }
+    }
+}
