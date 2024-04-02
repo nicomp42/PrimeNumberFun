ac39e7cc821786cd804c23c9ddf88d9ae83710bf
gubenohj <gubenohj@mail.uc.edu>
2024-04-02 11:26:18 -0400

    Added a constructor to a new class named after my 6+2

---
+++ PrimeNumberFun/gubenohj.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    internal class gubenohj
+    {
+        // Add a constructor that prints "Hello"
+        public gubenohj() {
+            Console.WriteLine("Hello");
+        }
+    }
+}
