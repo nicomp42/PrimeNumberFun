0307b08f931c6ef20c394d2b9cd859bd887b5cc9
Quynh Doan <doanqb@mail.uc.edu>
2024-04-02 11:26:31 -0400

    Add a constructor to the new class named after my 6+2

---
+++ PrimeNumberFun/doanqb.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    class doanqb
+    {
+        //Add a constructor that prints "Hello"
+        public doanqb()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
