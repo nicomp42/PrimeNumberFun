ce402866a132805575d74bdf0f67c28f771a3b48
Denise Huynh <huynhd2@mail.uc.edu>
2024-04-02 11:33:43 -0400

    Add a constructor to the new class after my 6+2

---
+++ PrimeNumberFun/huynhd2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    internal class huynhd2
+    {
+        // Add a constructor that prints "Hello"
+        public huynhd2()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
