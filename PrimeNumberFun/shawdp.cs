0ddf162b3a5779a07d0eb18b54c741f004095d7a
dobry1072 <shawdp@mail.uc.edu>
2024-04-02 11:43:53 -0400

    Added a class that was named after my 6+2

---
+++ PrimeNumberFun/shawdp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    internal class shawdp
+    {
+        public shawdp()
+        {
+            //Add a constructor that prints "Hello"
+            Console.WriteLine("Hello");
+        }
+    }
+}
