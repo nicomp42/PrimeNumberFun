f78ebc7449c41750fff65081b8a18413fc54de02
Mahika Gunjkar <gunjkamg@mail.uc.edu>
2024-04-02 11:26:28 -0400

    Add a contructor to the new class named after my 6+2

---
+++ PrimeNumberFun/gunjkamg.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    class gunjkamg
+    {// Add a contructor that prints "Hello"
+        public gunjkamg()
+        {
+            Console.WriteLine("Hello");
+        }
+
+    }
+}
