8bdd2eb6d6bfa6666cd59254acd5fc62e9f18717
Jackson Leaphart <leaphajp@mail.uc.edu>
2024-04-02 11:26:27 -0400

    Added a constructor to the new class named after my 6+2

---
+++ PrimeNumberFun/leaphajp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    internal class leaphajp
+    {
+        // Add a constructor that prints "Hello"
+        public leaphajp()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+}
