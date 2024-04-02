0e720ac271bbd7df493a9ad5094433e2b4f5bcde
Ben Ujvagi <ujvagibw@mail.uc.edu>
2024-04-02 11:26:26 -0400

    Add a constructor to the new class named after my 6+2

---
+++ PrimeNumberFun/Ujvagibw.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberFun
+{
+    internal class Ujvagibw
+    {
+        //Add a constructor that prints "Hello"
+        public Ujvagibw() {
+            Console.WriteLine("Hello");
+        }
+    }
+}
